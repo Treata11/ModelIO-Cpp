c6161f8356a7bb77fc4b545a83dd65d99f5209f9
Treata11 <treata11@yahoo.com>
2024-08-19 23:24:25 +0330

    Added `ModelIO.hpp`.

---
+++ ModelIO/ModelIO.hpp
@@ -0,0 +1,25 @@
+/**
+ @header    ModelIO.hpp
+ @framework ModelIO
+
+ Created by Treata Norouzi on 8/19/24.
+*/
+
+#import "ModelIOExports.hpp"
+
+#import "MDLAsset.hpp"
+#import "MDLAssetResolver.hpp"
+#import "MDLCamera.hpp"
+#import "MDLLight.hpp"
+#import "MDLMaterial.hpp"
+#import "MDLMesh.hpp"
+#import "MDLMeshBuffer.hpp"
+#import "MDLObject.hpp"
+#import "MDLSubmesh.hpp"
+#import "MDLTexture.hpp"
+#import "MDLTransform.hpp"
+#import "MDLTransformStack.hpp"
+#import "MDLTypes.hpp"
+#import "MDLVertexDescriptor.hpp"
+#import "MDLVoxelArray.hpp"
+#import "MDLAnimation.hpp"
